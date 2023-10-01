38a338f70f3e0bbe9c8488d3c0467c17589b9d1c
AWahab02 <awzahid02@gmail.com>
2023-10-01 19:17:04 +0500

    update

--- lib/itsamatch.dart
+++ lib/itsamatch.dart
@@ -11,25 +11,38 @@ class itsaMatch extends StatefulWidget {
 }
 
 class _itsaMatchState extends State<itsaMatch> {
+  bool _showContainer = false; // Control whether to show the container
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Trigger the slide-in animation after a delay (you can use any trigger mechanism)
+    Future.delayed(Duration(milliseconds: 500), () {
+      setState(() {
+        _showContainer = true;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 51, 51, 51),
       body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 100),
+        padding: const EdgeInsets.symmetric(vertical: 72),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24.0),
-              child: Image(
-                image: AssetImage("assets/tindertext.png"),
-              ),
-            ),
-            // Text(
-            //   "It's a Match!",
-            //   style: TextStyle(fontSize: 64, color: Colors.white),
-            // ),
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Text(
+                  "We're Engaged!",
+                  style: TextStyle(
+                      fontFamily: 'Rougescript',
+                      fontSize: 72,
+                      color: Colors.white),
+                )),
             SizedBox(height: 20),
             Text(
               "You and Ashen have liked each other",
@@ -42,72 +55,108 @@ class _itsaMatchState extends State<itsaMatch> {
             SizedBox(height: 35),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                CircleAvatar(
-                  backgroundColor: Colors.grey,
-                  radius: 72,
+              children: [
+                AnimatedContainer(
+                  duration: Duration(milliseconds: 400), // Animation duration
+                  curve: Curves.easeInOut, // Animation curve
+                  transform: Matrix4.translationValues(
+                    _showContainer ? 0.0 : 500.0, // Slide in from the right
+                    0.0,
+                    0.0,
+                  ),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.grey,
+                    radius: 72,
+                  ),
                 ),
                 SizedBox(
                   width: 20,
                 ),
-                CircleAvatar(
-                  backgroundColor: Colors.grey,
-                  radius: 72,
+                AnimatedContainer(
+                  duration: Duration(milliseconds: 400), // Animation duration
+                  curve: Curves.easeInOut, // Animation curve
+                  transform: Matrix4.translationValues(
+                    _showContainer ? 0.0 : -500.0, // Slide in from the right
+                    0.0,
+                    0.0,
+                  ),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.grey,
+                    radius: 72,
+                  ),
                 )
               ],
             ),
             SizedBox(height: 120),
-            Container(
-              width: 300,
-              height: 60,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  border: Border.all(color: Colors.white, width: 1.5)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Icon(
-                    FontAwesomeIcons.solidComment,
-                    color: Colors.white,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 12.0),
-                    child: Text(
-                      "Send a Message",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontFamily: 'Montserrat'),
+            AnimatedContainer(
+              duration: Duration(milliseconds: 500), // Animation duration
+              curve: Curves.easeInOut, // Animation curve
+              transform: Matrix4.translationValues(
+                _showContainer ? 0.0 : 500.0, // Slide in from the right
+                0.0,
+                0.0,
+              ),
+              child: Container(
+                width: 300,
+                height: 60,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(color: Colors.white, width: 1.5)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(
+                      FontAwesomeIcons.solidComment,
+                      color: Colors.white,
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 12.0),
+                      child: Text(
+                        "Send a Message",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontFamily: 'Montserrat'),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             SizedBox(height: 24),
-            Container(
-              width: 300,
-              height: 60,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  border: Border.all(color: Colors.white, width: 1.5)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Icon(
-                    FontAwesomeIcons.imagePortrait,
-                    color: Colors.white,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 12.0),
-                    child: Text(
-                      "Keep Playing",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontFamily: 'Montserrat'),
+            AnimatedContainer(
+              duration: Duration(milliseconds: 500), // Animation duration
+              curve: Curves.easeInOut, // Animation curve
+              transform: Matrix4.translationValues(
+                _showContainer ? 0.0 : -500.0, // Slide in from the right
+                0.0,
+                0.0,
+              ),
+              child: Container(
+                width: 300,
+                height: 60,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(color: Colors.white, width: 1.5)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(
+                      FontAwesomeIcons.imagePortrait,
+                      color: Colors.white,
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 12.0),
+                      child: Text(
+                        "Keep Playing",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontFamily: 'Montserrat'),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             SizedBox(height: 24),
--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -1,10 +1,28 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/itsamatch.dart';
 import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class MainPage extends StatelessWidget {
+bool swiped = false;
+
+class MainPage extends StatefulWidget {
+  MainPage({
+    super.key,
+  });
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  static double containerOpacity = 0; // Initialize opacity
+
+  @override
+  void initState() {
+    super.initState();
+    // You can initialize variables or perform other setup here
+  }
+
   List<Stack> cards = [
     Stack(
       alignment:
@@ -70,6 +88,32 @@ class MainPage extends StatelessWidget {
                 ),
               ],
             )),
+        Positioned(
+          left: 16, // Adjust the right position as needed
+          top: 100, // Adjust the top position as needed
+          child: Opacity(
+            opacity: containerOpacity,
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.green),
+                color: Colors
+                    .transparent, // You can change the background color as needed
+                borderRadius: BorderRadius.circular(
+                    8.0), // You can adjust the corner radius
+              ),
+              padding:
+                  EdgeInsets.all(8.0), // You can adjust the padding as needed
+              child: Text(
+                'I DO!',
+                style: TextStyle(
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Montserrat',
+                ),
+              ),
+            ),
+          ),
+        ),
       ],
     ),
     Stack(
@@ -81,61 +125,62 @@ class MainPage extends StatelessWidget {
           fit: BoxFit.cover, // Adjust the fit to your needs
         ),
         Positioned(
-            left: 16, // Adjust the left position as needed
-            bottom: 16, // Adjust the bottom position as needed
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: const [
-                    Text(
-                      'Ashen ',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 32),
-                    ),
-                    Text(
-                      '24',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w300,
-                          fontSize: 26),
-                    )
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: const [
-                    Icon(
-                      Icons.house_outlined,
-                      color: Colors.white,
-                      size: 16,
-                    ),
-                    Text(
-                      ' Lives in Colombo',
-                      style: TextStyle(color: Colors.white),
-                    )
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: const [
-                    Icon(
-                      Icons.location_on,
-                      color: Colors.white,
-                      size: 16,
-                    ),
-                    Text(
-                      ' less than a kilometre away',
-                      style: TextStyle(color: Colors.white),
-                    )
-                  ],
-                ),
-              ],
-            )),
+          left: 16, // Adjust the left position as needed
+          bottom: 16, // Adjust the bottom position as needed
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: const [
+                  Text(
+                    'Ashen ',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 32),
+                  ),
+                  Text(
+                    '24',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 26),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: const [
+                  Icon(
+                    Icons.house_outlined,
+                    color: Colors.white,
+                    size: 16,
+                  ),
+                  Text(
+                    ' Lives in Colombo',
+                    style: TextStyle(color: Colors.white),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: const [
+                  Icon(
+                    Icons.location_on,
+                    color: Colors.white,
+                    size: 16,
+                  ),
+                  Text(
+                    ' less than a kilometre away',
+                    style: TextStyle(color: Colors.white),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
       ],
     ),
     // Container(
@@ -149,12 +194,16 @@ class MainPage extends StatelessWidget {
     // ),
   ];
 
-  MainPage({
-    super.key,
-  });
-
   @override
   Widget build(BuildContext context) {
+    double opacity = 1.0;
+
+    void updateOpacity(double delta) {
+      setState(() {
+        containerOpacity = 1.0 - delta;
+      });
+    }
+
     bool _onSwipe(
         int previousIndex, int? currentIndex, CardSwiperDirection direction) {
       if (direction == CardSwiperDirection.right) {
@@ -184,8 +233,7 @@ class MainPage extends StatelessWidget {
 
     var phoneheight = MediaQuery.of(context).size.height;
     var phonewidth = MediaQuery.of(context).size.width;
-    var percentThresholdX = 90;
-    var percentThresholdy = 90;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 255, 251, 251),
@@ -218,9 +266,121 @@ class MainPage extends StatelessWidget {
               child: CardSwiper(
                   onSwipe: _onSwipe,
                   cardBuilder:
-                      (context, index, percentThresholdx, percentThresholdy) =>
-                          cards[index],
-                  cardsCount: cards.length)),
+                      (context, index, percentThresholdX, percentThresholdY) {
+                    var direction1;
+
+                    // Update the containerOpacity based on swipe progress
+                    if (percentThresholdX > 0) {
+                      containerOpacity =
+                          (percentThresholdX.abs() / 1000).clamp(0.0, 1.0);
+                    }
+
+                    if (percentThresholdX < 0) containerOpacity = 0;
+
+                    // print(containerOpacity);
+                    return Stack(
+                      alignment: Alignment
+                          .bottomLeft, // Position the text in the bottom-left corner
+                      children: <Widget>[
+                        Image.asset(
+                          'assets/pfp.jpeg', // Replace with the path to your image
+                          fit: BoxFit.cover, // Adjust the fit to your needs
+                        ),
+                        Positioned(
+                            left: 16, // Adjust the left position as needed
+                            bottom: 16, // Adjust the bottom position as needed
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: const [
+                                    Text(
+                                      'Ashen ',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 32),
+                                    ),
+                                    Text(
+                                      '24',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w300,
+                                          fontSize: 26),
+                                    )
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: const [
+                                    Icon(
+                                      Icons.house_outlined,
+                                      color: Colors.white,
+                                      size: 16,
+                                    ),
+                                    Text(
+                                      ' Lives in Colombo',
+                                      style: TextStyle(color: Colors.white),
+                                    )
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: const [
+                                    Icon(
+                                      Icons.location_on,
+                                      color: Colors.white,
+                                      size: 16,
+                                    ),
+                                    Text(
+                                      ' less than a kilometre away',
+                                      style: TextStyle(color: Colors.white),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            )),
+                        Positioned(
+                          left: 0, // Adjust the right position as needed
+                          top: 120, // Adjust the top position as needed
+                          child: Opacity(
+                            opacity: containerOpacity,
+                            child: Transform.rotate(
+                              angle: -3.14 / 3.75,
+                              child: Container(
+                                height: 80,
+                                width: 160,
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                      width: 8.0,
+                                      color: Color.fromARGB(255, 6, 255, 14)),
+                                  color: Colors
+                                      .transparent, // You can change the background color as needed
+                                  borderRadius: BorderRadius.circular(
+                                      8.0), // You can adjust the corner radius
+                                ),
+                                padding: EdgeInsets.all(
+                                    8.0), // You can adjust the padding as needed
+                                child: Text(
+                                  'I DO!',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      color: Color.fromARGB(255, 2, 255, 10),
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 42),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                    //return cards[index];
+                  },
+                  cardsCount: 2)),
           Expanded(
             flex: 1,
             child: Row(
